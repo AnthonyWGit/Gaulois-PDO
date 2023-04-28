82ea563124654ae7ebf0dc10f7dd7feae06106e3
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-28 08:51:32 +0200

    connection too sql serv with PDO

--- index.php
+++ index.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+    <?php 
+    try
+    {
+        $mySQLconnection = new PDO(                                                     //Connecting to SQL server
+            'mysql:host=127.0.0.1;dbname=gaulois;charset=utf8',
+            'root',
+            '',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);                              
+    }
+    catch (Exception $e) //catching errors                                              //Show error if we can't connect
+    {
+        die('Erreur : ' . $e->getMessage());
+    }    
+
+    ?>
+    </div>
 </body>
 </html>
